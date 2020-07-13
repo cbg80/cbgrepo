b19add2cbd28c355110ba2e801cb0e7f22dc2cb6
cbg80 <cbg80@cbg80-aspirea31554>
2020-07-13 12:07:16 +0200

    docs: document script

--- ImageThread/tests/autoload.php
+++ ImageThread/tests/autoload.php
@@ -1,2 +1,10 @@
 <?php
+/**
+ * Bootstrapping script of the app tests. Tells PHPUnit which preparation steps to take before testing
+ * 
+ * @author Carlos Blanco Gañán <carlos.blanga@gmail.com>
+ */
+/**
+ * Loads composer default autoloader
+ */
 require_once __DIR__ . '/../vendor/autoload.php';
