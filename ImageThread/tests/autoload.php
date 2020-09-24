64057ae1b4fe2380386e0ef84d21b0db320e5ef4
cbg80 <cbg80@cbg80-aspirea31554>
2020-09-24 11:40:29 +0200

    test: update the bootstrapping declaration
    Import the definition of the app global constants
    along with the composer default autoloader.
    Get some of the global constants updated before running the app unit
    tests

--- ImageThread/tests/autoload.php
+++ ImageThread/tests/autoload.php
@@ -8,3 +8,6 @@
  * Loads composer default autoloader
  */
 require_once __DIR__ . '/../vendor/autoload.php';
+
+require_once __DIR__ . '/../config/app_paths.php';
+require_once __DIR__ . '/../config/ddbb_credentials.php';
